2924305d2178b042c7ce563a2348ebd5ee773c06
Cenax <edichasan75@gmail.com>
2024-07-13 02:47:25 +0900

    Added fixed navbar

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import HeaderNavbar from "./components/Header-Navbar.vue";
 </script>
 
 <template>
-    <div class="flex flex-col h-screen w-">
-        <HeaderNavbar class="sticky top-0 z-[5]" />
-        <router-view></router-view>
+    <div class="flex flex-col h-screen w-full">
+        <HeaderNavbar class="fixed z-[5]" />
+        <router-view class="relative pt-[5.5rem]"></router-view>
     </div>
 </template>
