b008e5ba3484ee197327066a373dcd8f237d4f5b
Cenax <edichasan75@gmail.com>
2024-07-12 22:45:35 +0900

    Added placeholder card in the auth page

--- src/Pages/AuthPage.vue
+++ src/Pages/AuthPage.vue
@@ -1,5 +1,9 @@
 <template>
-    <div>
-        <h1>auth</h1>
+    <div class="flex items-center justify-center h-full w-full">
+        <div
+            class="Card flex w-1/2 h-1/2 bg-base-300 rounded-xl p-3 justify-center"
+        >
+            <h1 class="text-3xl font-bold">Authorization</h1>
+        </div>
     </div>
 </template>
