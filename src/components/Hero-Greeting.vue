f34139b5cd04eae7244d5af267893c203d8dafaf
Cenax <edichasan75@gmail.com>
2024-07-14 00:29:32 +0900

    Updated Hero-Greeting text

--- src/components/Hero-Greeting.vue
+++ src/components/Hero-Greeting.vue
@@ -6,17 +6,17 @@
             <div class="max-w-md">
                 <h1 class="text-5xl font-bold">Testing playground</h1>
                 <p class="py-6">
-                    This is a testing playground. Feel free to use it to test
-                    new components and features. Try to use components to keep
-                    everything clean pls. <br />
+                    This is a testing playground.<br />
                     <b
-                        >Idk try to use blocks for the main components that
-                        should be displayed in the main page (Header, Hero,
-                        etc.) otherwise is causes some strange behavior in
-                        layout
+                        >Idk how scaling works but use flex whenever possible
                     </b>
                     <br />
-                    <b class="text-red-500">Or don't idk how it works</b>
+                    <b class="text-red-500">Supabase test page doesn't work on mobile</b>
+                    <b class="text-red-500">No idea how to fetch data from supabase nor how to display it</b>
+                    <b class="text-red-500">Authorization it's ready, only the base form is ready, maybe use Auto0, SuperTokens or maybe supabase solution if there is any</b>
+                    <p class="py-6">
+                        Keep GSAP.com and threejs in mind
+                    </p>
                 </p>
                 <router-link to="/components">
                     <button class="btn btn-primary btn-outline">
